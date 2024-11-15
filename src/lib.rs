2c20f5aa3f80469fdab968b7dbaa4e0f61ef3e66
keithyin <yinpenghhz@hotmail.com>
2024-11-15 22:01:25 +0800

    query2ref

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,56 @@
 pub mod cli;
-mod samtools;
+pub mod fille_reader;
+use std::thread;
+
 use cli::{AlignArgs, IndexArgs, MapArgs};
+use crossbeam::channel::{Receiver, Sender};
+use fille_reader::{FastaFileReader, QueryRecord};
 use minimap2::{Aligner, Preset};
 use rust_htslib::bam::Read;
 
-fn build_aligner(preset: &str, index_args: &IndexArgs, map_args: &MapArgs, align_args: &AlignArgs) -> Aligner{
+fn build_aligner(
+    preset: &str,
+    index_args: &IndexArgs,
+    map_args: &MapArgs,
+    align_args: &AlignArgs,
 
-    let mut aligner = Aligner::builder();
-    aligner = match preset {
-        "map-ont" => aligner.map_ont(),
-        ps => panic!("invalid preset {}", ps)
-    };
+    targets: &Vec<QueryRecord>,
+) -> Vec<Aligner> {
+    let aligners = thread::scope(|s| {
+        let mut handles = vec![];
+        for target in targets {
+            let hd = s.spawn(|| {
+                let mut aligner = Aligner::builder();
 
+                aligner = match preset {
+                    "map-ont" => aligner.map_ont(),
+                    "map-pb" => aligner.map_pb(),
+                    "map-hifi" => aligner.map_hifi(),
 
-    aligner
+                    pre => panic!("not implemented yet {}", pre),
+                };
 
+                aligner = aligner
+                    .with_index_threads(4)
+                    .with_cigar()
+                    .with_sam_out()
+                    .with_sam_hit_only()
+                    .with_seq_and_id(target.sequence.as_bytes(), target.qname.as_bytes())
+                    .unwrap();
+
+                aligner
+            });
+            handles.push(hd);
+        }
+
+        handles
+            .into_iter()
+            .map(|hd| hd.join().unwrap())
+            .collect::<Vec<Aligner>>()
+    });
+
+    aligners
 }
-
 
 pub fn index_ref_file() {}
 
@@ -26,61 +60,55 @@ pub fn query2ref_align(
     indexed_ref_file: Option<&str>,
     oup_filename: &str,
     mut aligner: Aligner,
-    threads: usize
+    threads: usize,
 ) {
     if ref_file.is_none() && indexed_ref_file.is_none() {
         panic!("ref_file and indexed_ref_file cannot all be none");
     }
 
     if indexed_ref_file.is_some() {
-        
     } else {
-        // aligner.with_index_threads(threads).with_seq(seq)
     }
-
-    
 }
 
-pub fn subreads2smc_align(subreads_bam: &str, smc_bam: &str) {
+pub fn query_seq_sender(filenames: &Vec<String>, sender: Sender<QueryRecord>) {
+    for filename in filenames {
+        if filename.ends_with("fa") || filename.ends_with("fasta") {
+            let fasta_reader = FastaFileReader::new(filename.clone());
 
-    let sorted_subreads_bam = samtools::sort(subreads_bam, "ch");
-    let sorted_smc_bam = samtools::sort(smc_bam, "ch");
-
-    let mut smc_bam_reader = rust_htslib::bam::Reader::from_path(&sorted_smc_bam).unwrap();
-    smc_bam_reader.set_threads(5).unwrap();
-    let mut subreads_bam_reader = rust_htslib::bam::Reader::from_path(&sorted_subreads_bam).unwrap();
-    subreads_bam_reader.set_threads(5).unwrap();
-
-    
-    let mut smc_records = smc_bam_reader.records();
-    let mut subreads_records = subreads_bam_reader.records();
-
-    loop {
-        
-        let smc_record = smc_records.next();
-        if smc_record.is_none() {
-            break;
-        }
-
-        let smc_record = smc_record.unwrap().unwrap();
-
-
-        let mut sbr_record = None;
-
-        loop {
-            if sbr_record.is_none() {
-                sbr_record = subreads_records.next();
+            for record in fasta_reader {
+                sender.send(record).unwrap();
             }
-
-            let sbr = sbr_record.unwrap().unwrap();
-            
-
-            sbr_record = subreads_records.next();
+        } else if filename.ends_with("bam") {
+            let mut bam_h = rust_htslib::bam::Reader::from_path(filename).unwrap();
+            bam_h.set_threads(4).unwrap();
+            for record in bam_h.records() {
+                let record = record.unwrap();
+                sender.send(QueryRecord::from_bam_record(&record)).unwrap();
+            }
         }
-
     }
-
 }
+
+pub fn aligner(query_record_recv: Receiver<QueryRecord>, aligners: &Vec<Aligner>) {
+    for query_record in query_record_recv {
+        for aligner in aligners {
+
+            for hit in aligner
+                .map(
+                    query_record.sequence.as_bytes(),
+                    true,
+                    true,
+                    None,
+                    Some(&[67108864]), // 67108864 eqx
+                )
+                .unwrap()
+            {}
+        }
+    }
+}
+
+pub fn writer() {}
 
 #[cfg(test)]
 mod tests {
