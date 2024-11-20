8e7b29fae8f0d25d5c5653618b2c0ea65ad7cf6c
keith <yinpenghhz@hotmail.com>
2024-11-20 10:20:49 +0800

    sort

--- src/samtools.rs
+++ src/samtools.rs
@@ -2,39 +2,6 @@ use std::process::Command;
 
 use std::{fs, path, process};
 
-pub fn sort(bam_file: &str, tag: &str, threads: Option<usize>) -> String {
-    let threads = threads.unwrap_or(num_cpus::get_physical() / 2);
-    let threads = if threads > 1 { threads } else { 1 };
-
-    let out_bam = format!(
-        "{}.sort_by_{}.bam",
-        bam_file.rsplit_once(".").unwrap().0,
-        tag
-    );
-
-    let mut cmd = Command::new("samtools");
-    cmd.args([
-        "sort",
-        "-@",
-        threads.to_string().as_str(),
-        "-n",
-        "-t",
-        tag,
-        "-o",
-        out_bam.as_str(),
-    ]);
-
-    let oup = cmd.output().expect(&format!("sort {} error", bam_file));
-    if !oup.status.success() {
-        panic!(
-            "sort {} error. {}",
-            bam_file,
-            String::from_utf8(oup.stderr).unwrap()
-        );
-    }
-
-    out_bam
-}
 
 pub fn samtools_bai(bam_file: &str, force: bool, threads: Option<usize>) -> anyhow::Result<()> {
     let res_filepath = format!("{}.bai", bam_file);
@@ -75,13 +42,13 @@ pub fn sort_by_coordinates(bam_file: &str, threads: Option<usize>) {
     let threads = threads.unwrap_or(num_cpus::get_physical() / 2);
     let threads = if threads > 1 { threads } else { 1 };
 
-    let out_bam = format!("{}.tmp", bam_file);
+    // let out_bam = format!("{}.tmp", bam_file);
 
     let mut cmd = Command::new("samtools");
     cmd.args([
         "sort",
         "-o",
-        out_bam.as_str(),
+        bam_file,
         "-@",
         threads.to_string().as_str(),
         bam_file,
@@ -96,5 +63,5 @@ pub fn sort_by_coordinates(bam_file: &str, threads: Option<usize>) {
         );
     }
 
-    fs::rename(out_bam, bam_file).unwrap();
+    // fs::rename(out_bam, bam_file).unwrap();
 }
